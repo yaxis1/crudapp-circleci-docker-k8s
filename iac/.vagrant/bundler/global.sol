6701b3191bfb3a551fbdd6331140407fa793cffb
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2020-11-12 17:51:42 +0100

    Vagrant
    Initiating vagrant file

---
+++ iac/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.24.0"]]],"checksum":"8c46eacc3791aefb23e967d09111a70496dc021b6066f1fb1fab829269d542f7","vagrant_version":"2.2.10"}
